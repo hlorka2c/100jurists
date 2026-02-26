8ec00ebc4109dd2d51d6a35310846062a24d6060
unknown <arbuzzzzz44@mail.ru>
2026-02-26 13:22:25 +0300

    ready adaptive, scripts, menu, popup

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,4 +1,5 @@
 const TEXT_STATES = ["140px", "auto"];
+const LEFT__VARIANTS = [31, 16, 24];
 const BUTTON_STATES = ["Показать весь текст", "Свернуть"];
 
 const containerNode = document.querySelector(".container");
@@ -9,6 +10,19 @@ const sliderRight = document.querySelector(".slider-right");
 const sliderLeft = document.querySelector(".slider-left");
 const sliderItem = document.querySelector(".reviews__list__item");
 const juristsMainNode = document.querySelector(".jurists__main-content");
+const burgerMenu = document.querySelector(".burger-menu");
+const menu = document.querySelector(".menu");
+const menuBackground = document.querySelector(".menu__background");
+const menuClose = document.querySelector(".menu__close");
+const searchMobileButton = document.querySelector(".header-mobile__buttons__search");
+const searchMobile = document.querySelector(".search-mobile");
+const searchCloseButton = document.querySelector(".search__close");
+const popupJuristsList = document.querySelectorAll(".popup__jurists-list__item");
+const popupButton = document.querySelector(".main__actions-button");
+const popup = document.querySelector(".popup");
+const questionButton = document.querySelector(".question");
+const popupCloseButton = document.querySelector(".popup__close");
+const actionButtons = document.querySelectorAll(".tutorial__variants__item-action");
 
 let leftValue = 0,
     textState = 0,
@@ -16,6 +30,7 @@ let leftValue = 0,
     sliderCurrentPos = 1,
     sliderEndPos = juristsList.length,
     containerWidth = 0,
+    desctopFormat = true,
     list,
     gap;
 
@@ -23,6 +38,10 @@ let leftValue = 0,
 
 document.addEventListener("DOMContentLoaded", () => {
     juristsList.forEach(setLeft);
+    leftValue = 0;
+
+    popupJuristsList.forEach(setLeft);
+    leftValue = 0;
 
     setContainerWidth();
 
@@ -41,7 +60,72 @@ document.addEventListener("DOMContentLoaded", () => {
         // листаем вправо
         slide("right");
     })
+
+    burgerMenu.addEventListener("click", () => {
+        showMenu();
+    })
+
+    menuClose.addEventListener("click", () => {
+        hideMenu();
+    })
+
+    popupButton.addEventListener("click", () => {
+        popup.classList.add("active");
+        menuBackground.classList.add("active");
+    });
+
+    questionButton.addEventListener("click", () => {
+        popup.classList.add("active");
+        menuBackground.classList.add("active");
+    });
+
+    actionButtons.forEach((item) => {
+        item.addEventListener("click", () => {
+            popup.classList.add("active");
+            menuBackground.classList.add("active");
+        });
+    })
+
+    popupCloseButton.addEventListener("click", () => {
+        popup.classList.remove("active");
+        menuBackground.classList.remove("active");
+    })
+
+    window.addEventListener("resize", () => {
+        if (window.innerWidth <= 1080 && desctopFormat === true) {
+            desctopFormat = false;
+            juristsList.forEach(setLeft);
+            console.log("resize to mobile format");
+        } else if (window.innerWidth > 1080 && desctopFormat === false) {
+            desctopFormat = true;
+            juristsList.forEach(setLeft);
+            console.log("resize to desc format")
+        }
+        leftValue = 0;
+    });
+
+    searchMobileButton.addEventListener("click", () => {
+        searchMobile.classList.add("active");
+        menuBackground.classList.add("active");
+    })
+
+    searchCloseButton.addEventListener("click", () => {
+        searchMobile.classList.remove("active");
+        menuBackground.classList.remove("active");
+    })
 });
+
+function showMenu() {
+    menuBackground.classList.add("active");
+    menu.classList.add("active");
+    document.body.style.overflow = "hidden";
+}
+
+function hideMenu() {
+    menuBackground.classList.remove("active");
+    menu.classList.remove("active");
+    document.body.style.overflow = "";
+}
 
 function addPX(value) {
     return value + "px";
@@ -53,7 +137,9 @@ function setContainerWidth() {
 
 function setLeft(element) {
     element.style.left = addPX(leftValue);
-    leftValue -= 31;
+    let itPopup = element.classList.contains("popup__jurists-list__item") || element.classList.contains("popup__jurists-list__more");
+
+    leftValue -= itPopup ? 20 : window.innerWidth > 1080 ? 31 : 16;
 }
 
 function toggleText(element) {
